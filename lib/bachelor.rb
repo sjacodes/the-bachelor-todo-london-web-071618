cd14121d5163c9a8320fc74b4a6a68fb85ca723a
Sarah Jacob <smjacob91@gmail.com>
2018-07-08 09:49:07 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,53 @@
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+ data.each do |season, contestants_array|
+    contestants_array.find do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
+
+
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+    data.each do |season, contestants_array|
+      contestants_array.each do |contestant|
+        if contestant["hometown"] == hometown
+          count = count + 1
+        end
+      end
+    end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants_array|
+    contestants_array.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
+
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_ages = data[season].collect do |contestant|
+    contestant["age"].to_i
+  end
+  sum_of_ages = contestant_ages.reduce(0) {|sum, age| sum += age}
+  contestant_count =  contestant_ages.count.to_f
+  return (sum_of_ages/contestant_count).round
 end
